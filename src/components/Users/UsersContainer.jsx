6e38ebb997d4947ec67b25f3bfc5448835b54ff4
sergey <zhuravkov.ss@yandex.ru>
2022-04-20 15:40:04 +0500

    81 - сделал селекторы для users

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,6 +10,7 @@ import Preloader from '../common/Preloader';
 import { getUsersThunkCreator } from './../../redux/usersReduser';
 import { compose } from 'redux';
 import { withAuthRedirect } from '../hoc/withAuthRedirect';
+import { getPageSize, getUsers, getTotalUsersCount, getCurrentPage, getIsFetching, getFollowingInProgress } from '../../redux/usersSelectors';
 
 
 class UsersContainer extends React.Component{
@@ -45,44 +46,16 @@ class UsersContainer extends React.Component{
 let mapStateToProps = (state) => {
 
     return {
-        users: state.usersPage.users,
-        pageSize : state.usersPage.pageSize,
-        totalUsersCount : state.usersPage.totalUsersCount,
-        currentPage : state.usersPage.currentPage,
-        isFetching : state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
+        users: getUsers (state),
+        pageSize: getPageSize (state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state), 
+        followingInProgress: getFollowingInProgress(state)
 
     }
 }
 
-/*
-let MapDispatchToProps = (dispatch) => {
-
-    return {
-        follow: (userId) => {
-           
-            dispatch(followAC(userId));
-        },
-        unfollow: (userId) => {
-            dispatch( unfollowAC(userId));
-        },
-        setUsers: (users) => {
-            
-            dispatch(setUsersAC(users));
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(setCurrentPageAC(pageNumber));
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setTotalUsersCountAC(totalCount));
-        },
-        toggleIsFething: (isFetching) => {
-            dispatch(toggleIsFethingAC(isFetching))
-        }
-
-    }
-}
-*/
 export default compose (
     
     connect(mapStateToProps, {follow, unfollow, setCurrentPage,
