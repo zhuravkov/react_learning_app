c70cf64c4a80ce71aa87c05adb77164c869fe463
sergey <zhuravkov.ss@yandex.ru>
2022-04-20 22:26:32 +0500

    85 HOOK useEffect()

--- src/components/Profile/ProfileInfo/Status/StatusWithHOOK.jsx
+++ src/components/Profile/ProfileInfo/Status/StatusWithHOOK.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const StatusWithHOOK = (props) =>  {
 
     let [editMode, setEditMode] = useState (false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect( () =>{
+        setStatus(props.status); 
+        // подписываемся на изменение значения
+    }, [props.status]);
+
 
     let activateEditMode = () => {
         setEditMode(true)
